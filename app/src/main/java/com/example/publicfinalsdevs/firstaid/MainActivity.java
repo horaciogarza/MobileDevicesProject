e304b59ad5b1838588acda8258793f4bb122141e
Horacio <hgx95@hotmail.com>
2016-01-28 00:30:22 -0600

    Rules Added

--- app/src/main/java/com/example/publicfinalsdevs/firstaid/MainActivity.java
+++ app/src/main/java/com/example/publicfinalsdevs/firstaid/MainActivity.java
@@ -1,5 +1,28 @@
 package com.example.publicfinalsdevs.firstaid;
 
+/**
+ * LAST UPDATE OF RULES: 28/01/2016 BY: HORACIO
+ *
+ * NOTES:
+ *      <NOTES FOR THE NEXT AUTHOR WHO WILL UPDATE HIS FILE GOES HERE>
+ *
+ * Rules of this Project:
+ *
+ * 1. Do not abuse from pushing into the GitHub Repo, only when you do more than 10 changes (or lees if it is necessary)
+ * 2. When you push it into GitHub always use an Author
+ *
+ *          Horacio ->  HGarz
+ *          Omar ->     FILL HERE
+ *          Diego ->    FILL HERE
+ *
+ * 3. Be careful what you move in the code, remember if you open it again, try to update it always, someone might made changes
+ * before
+ *
+ * 4. BE CAREFUL WITH THE GRADLE AND MAVEN!
+ *
+ *
+ *
+ */
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -10,6 +33,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("hallo");
+
     }
 }
