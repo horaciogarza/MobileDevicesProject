83c3ad89e25662b499aa8794f2cfc36f52e46c93
Horacio Garza <hgx95@hotmail.com>
2016-02-11 08:50:17 -0600

    Update EmergencyPhoneNumDBSchema.java
    Added some methods to the databases java files.

--- app/src/main/java/database/EmergencyPhoneNumDBSchema.java
+++ app/src/main/java/database/EmergencyPhoneNumDBSchema.java
@@ -5,7 +5,7 @@ package database;
  */
 public class EmergencyPhoneNumDBSchema {
 
-    public static final class EmergencyPhoneNumbers {
+    public static final class EmergencyPhoneNumTable {
 
         public static final String NAME = "Tbl_EmergencyPhoneNumbers";
 
@@ -13,6 +13,20 @@ public class EmergencyPhoneNumDBSchema {
 
             public static final String NAME = "name";
             public static final String PHONE_NUMBER = "number";
+            
+            public static final String[] COL = {"name", "number"}; 
+28 
+ 
+29             public static String getCreateQueryCols(){ 
+30                 String query =""; 
+31                 for (int i=0 ; i<COL.length ; i++){ 
+32                     query = query + COL[i] + " text "; 
+33                     if(i<COL.length-1) 
+34                         query = query+","; 
+35                 } 
+36                 return query; 
+37             } 
+
         }
     }
 }
