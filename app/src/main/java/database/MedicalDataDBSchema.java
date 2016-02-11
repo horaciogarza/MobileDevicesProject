b9480de2bf20f99ed377b67b3985796c80605735
Horacio Garza <hgx95@hotmail.com>
2016-02-11 08:54:02 -0600

    Update MedicalDataDBSchema.java
    Added methods to the database's package classes

--- app/src/main/java/database/MedicalDataDBSchema.java
+++ app/src/main/java/database/MedicalDataDBSchema.java
@@ -18,6 +18,20 @@ public class MedicalDataDBSchema {
             public static final String POLICY_NUMBER = "policyNumber";
             public static final String DOCTORS_NAME = "doctorsName";
             public static final String OFFICE_NUMBER = "officeNumber";
+            
+            public static final String[] COL = {"insuranceProvider", "location", "policyNumber", "doctorsName", "officeNumber"}; 
+28 
+ 
+29             public static String getCreateQueryCols(){ 
+30                 String query =""; 
+31                 for (int i=0;i<COL.length;i++){ 
+32                     query = query + COL[i] + " text "; 
+33                     if(i<COL.length-1) 
+34                         query = query+","; 
+35                 } 
+36                 return query; 
+37             } 
+
         }
     }
 
