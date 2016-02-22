6141071c110a1592a29a13941418d6b2cf19195d
Horacio <hgx95@hotmail.com>
2016-02-22 01:16:33 -0600

    Form added, BUGS EVERYWHERE

--- app/src/main/java/com/example/publicfinalsdevs/firstaid/ScrollingActivity.java
+++ app/src/main/java/com/example/publicfinalsdevs/firstaid/ScrollingActivity.java
@@ -7,6 +7,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.rey.material.widget.EditText;
+
+import java.util.ArrayList;
+
 public class ScrollingActivity extends AppCompatActivity {
 
     @Override
@@ -16,13 +20,31 @@ public class ScrollingActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        ArrayList<EditText> fieldString = new ArrayList<EditText>(20);
+
+        EditText nameField = (EditText) findViewById(R.id.nameField);
+        EditText addressField = (EditText) findViewById(R.id.addressField);
+        EditText cityField = (EditText) findViewById(R.id.cityField);
+        EditText emailField = (EditText) findViewById(R.id.emailField);
+        EditText homePhoneField = (EditText) findViewById(R.id.homePhoneField);
+        EditText zipCodeField = (EditText) findViewById(R.id.zipCodeField);
+        EditText mobileField = (EditText) findViewById(R.id.mobileField);
+
+
+
+
+
+        nameField.setTextSize(20);
+
+
+
+//
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
     }
 }
